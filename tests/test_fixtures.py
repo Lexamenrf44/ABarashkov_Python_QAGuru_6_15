4235dea4c176ecb4d779ea78645d0cabd90c0f1f
ABarashkov <alexander.stcstudent@gmail.com>
2023-08-05 21:38:58 +0400

    init commit - task done

---
+++ tests/test_fixtures.py
@@ -0,0 +1,38 @@
+import pytest
+from selene import browser, have
+
+
+"""
+Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
+"""
+
+
+@pytest.fixture(scope='function', autouse=False, params=[(1366, 768), (1920, 1080)])
+def desktop_browser(request):
+    browser.config.base_url = 'https://github.com'
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+    yield
+    browser.quit()
+
+
+@pytest.fixture(scope='function', autouse=False, params=[(375, 667), (390, 844)])
+def mobile_browser(request):
+    browser.config.base_url = 'https://github.com'
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+    yield
+    browser.quit()
+
+
+def test_github_desktop(desktop_browser):
+    browser.open('/')
+    browser.element("a[href='/login']").click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+
+
+def test_github_mobile(mobile_browser):
+    browser.open('/')
+    browser.element('.flex-1 button').click()
+    browser.element("a[href='/login']").click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
